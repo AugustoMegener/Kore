65ae4f038c12d7da3bc1409e4cfd0b83ed75b726
AugustoMegener <vcgolem@gmail.com>
2025-05-19 16:02:56 -0300

    gro

--- ksp/build.gradle.kts
+++ ksp/build.gradle.kts
@@ -1,4 +1,7 @@
+inline val String.prop get() = project.findProperty(this) as String
+
 plugins {
+    `maven-publish`
     kotlin("jvm") version "2.1.0"
     id("com.google.devtools.ksp") version "2.1.0-1.0.29"
 
@@ -6,7 +9,7 @@ plugins {
 }
 
 group = "io.kito.kore_ksp"
-version = "0.0.1"
+version = "mod_version".prop
 
 java.toolchain.languageVersion = JavaLanguageVersion.of(21)
 
@@ -31,6 +34,28 @@ dependencies {
     ksp("com.google.dagger:dagger-compiler:2.51.1")
 
     implementation("com.google.devtools.ksp:symbol-processing-api:2.1.0-1.0.29")
+}
+
+publishing {
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/AugustoMegener/Kore")
+            credentials {
+                username = System.getenv("GITHUB_ACTOR")
+                password = System.getenv("GITHUB_TOKEN")
+            }
+        }
+    }
+    publications {
+        register<MavenPublication>("gpr") {
+            groupId = "mod_group_id".prop
+            artifactId = "ksp"
+            version = "processor_version".prop
+
+            from(components["java"])
+        }
+    }
 }
 
 tasks.test {
