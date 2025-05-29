fa2cd4d85fc75092274790b4c86ae5e39340f42f
AugustoMegener <vcgolem@gmail.com>
2025-05-29 13:31:23 -0300

    [Macen]

--- src/main/kotlin/io/kito/kore/common/registry/SimpleRegister.kt
+++ src/main/kotlin/io/kito/kore/common/registry/SimpleRegister.kt
@@ -1,13 +1,16 @@
 package io.kito.kore.common.registry
 
 import net.minecraft.core.Registry
+import net.minecraft.resources.ResourceKey
 import net.neoforged.bus.api.IEventBus
 import net.neoforged.neoforge.registries.DeferredHolder
 import net.neoforged.neoforge.registries.DeferredRegister
 
-open class SimpleRegister<T>(final override val id: String, registry: Registry<T>) : AutoRegister {
+open class SimpleRegister<T>(private val register: DeferredRegister<T>, override val id: String) : AutoRegister {
 
-    private val register = DeferredRegister.create(registry, id)
+    constructor(id: String, registry: Registry<T>) : this(DeferredRegister.create( registry, id), id)
+
+    constructor(id: String, resourceKey: ResourceKey<Registry<T>>): this(DeferredRegister.create(resourceKey, id), id)
 
     operator fun <O : T> String.invoke(supplier: () -> O): DeferredHolder<T, O> = register.register(this, supplier)
 
